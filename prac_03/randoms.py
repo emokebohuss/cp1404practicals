7c1489f352e7cab5f397df4491a8671de5ea380e
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-10-04 20:30:15 +1000

    Add randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,31 @@
+"""
+Prac_03
+randoms.py
+"""
+# Q1
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+
+# Answer: smallest possible integer is 5 and the largest is 20.
+
+
+# Q2
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+
+# Answer: smallest possible integer is 3 and the largest is 9.
+# 4 cannot be produced because the program prints integers in steps of 2, starting from 3,
+# therefore only odd numbers are possible (ie. 3, 5, 7, 9)
+
+
+# Q3
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# Answer: smallest possible is 2.5 and largest possible is 5.5
+
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive
+import random
+
+print(random.randint(1, 100))
