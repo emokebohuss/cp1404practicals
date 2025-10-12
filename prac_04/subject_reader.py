c727f9d9ab5d15fcaccfbb6428aa182d9c4c1942
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-10-12 20:25:12 +1000

    Add load_subject_records() and display_subject_details() functions to subject_reader.py

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,33 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    subject_records = load_subject_records(FILENAME)
+    print(subject_records)
+    display_subject_details(subject_records)
 
 
-def load_data(filename=FILENAME):
+def load_subject_records(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject_records = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_details = line.split(',')  # Separate the data into its parts
+        # print(parts)  # See what the parts look like (notice the integer is a string)
+        subject_details[2] = int(subject_details[2])  # Make the number an integer (ignore PyCharm's warning)
+        # print(parts)  # See if that worked
+        # print("----------")
+        subject_records.append(subject_details)
     input_file.close()
+    return subject_records
+
+
+def display_subject_details(subject_records):
+    """Print subject details."""
+    for subject in subject_records:
+        print("{} is taught by {:12} and has {:3} students".format(*subject))
 
 
 main()
