f60e498dc25b27b6c1244e310e3fd9e9eda884ce
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-10-17 15:08:39 +1000

    Sort output with sorted function instead of using sort method on keys in word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,12 +5,11 @@ Actual: 13 minutes
 """
 
 text = input("Text: ")
+# text = "this is a collection of words of nice words this is a fun thing it is"  # Test
 words = text.split()
-words.sort()
-print(words)
-max_length = max([len(word) for word in words])
+max_length = max(len(word) for word in words)
 word_to_count = {}
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
-for word in word_to_count:
+for word in sorted(word_to_count):
     print(f"{word:{max_length}} :{word_to_count[word]:2}")
