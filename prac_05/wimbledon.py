a849a840403a894d81c3e68f308cb501f6d53725
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-10-20 16:06:02 +1000

    Refactor wimbledon.py into 4 functions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,34 +1,73 @@
-"""Write a program to read this file, process the data and display processed information:
+"""Write a program to read this file, process the matches and display processed information:
 
 the champions and how many times they have won.
 the countries of the champions in alphabetical order
 Estimated: 45 mins
-Actual:
+Actual: 68 mins
 """
 
+# import csv
 
-filename = "wimbledon.csv"
+FILENAME = "wimbledon.csv"
 
-champion_to_wins = {}
-data = []
-champions_nationalities = []
-with open(filename, "r", encoding="utf-8-sig") as in_file:
-    in_file.readline()
-    for line in in_file:
-        line = line.strip()
-        match_details = line.split(",")
-        match_details[0] = int(match_details[0])  # Ignore error
-        data.append(match_details)
-        nationality = match_details[1]
-        champions_nationalities.append(nationality)
-        champion = match_details[2]
+
+def main():
+    """Load matches data from file, print champions and their number of wins, and the set of champions' countries"""
+    matches = load_matches_data(FILENAME)
+    champion_to_wins = create_champion_to_wins(matches)
+
+    print("Wimbledon Champions:")
+    for champion in champion_to_wins:
+        print(champion, champion_to_wins[champion])
+
+    champions_countries = find_champions_countries(matches)
+    print(f"\nThese {len(champions_countries)} countries have won Wimbledon:")
+    print(", ".join(champions_countries))
+
+
+def load_matches_data(filename):
+    """Load matches from file and return formatted list of matches."""
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        matches = []
+        in_file.readline()
+        for line in in_file:
+            line = line.strip()
+            match_details = line.split(",")
+            match_details[0] = int(match_details[0])  # ignore error, convert year to int
+            match_details[5] = ",".join(match_details[5:])  # create sting of scores
+            del match_details[6:]  # delete redundant scores
+            matches.append(match_details)
+        return matches
+
+
+# def load_matches_data(filename):  # csv reader version. Uncomment import if ran and comment out other version of func
+#     """Using csv module, load matches from file and return list of matches."""
+#     with open(filename, "r", encoding="utf-8-sig") as in_file:
+#         matches = []
+#         rows = csv.reader(in_file)
+#         next(rows)  # remove header
+#         for row in rows:
+#             row[0] = int(row[0])  # Ignore error, convert year to int
+#             matches.append(row)
+#         return matches
+
+
+def create_champion_to_wins(matches):
+    """Create dictionary of champions and their number of wins."""
+    champion_to_wins = {}
+    for match in matches:
+        champion = match[2]
         champion_to_wins[champion] = champion_to_wins.get(champion, 0) + 1
+    return champion_to_wins
 
 
-print("Wimbledon Champions:")
-for champion in champion_to_wins:
-    print(champion, champion_to_wins[champion])
+def find_champions_countries(matches):
+    """Create an alphabetically sorted set of champions' countries."""
+    champions_countries = set()
+    for match in matches:
+        country = match[1]
+        champions_countries.add(country)
+    return sorted(champions_countries)
 
-print()
-print("These 12 countries have won Wimbledon:")
-print(", ".join(sorted(set(champions_nationalities))))
+
+main()
