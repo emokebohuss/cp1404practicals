d2758ac57bd4e355200263109f21afd5f07d1a38
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-10-20 16:38:26 +1000

    Remove unnecessary spaces from amber's hex_code in the dictionary

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,8 +2,8 @@
 Hexadecimal colour code lookup
 """
 
-COLOUR_TO_HEX = {"absolute zero": "#0048ba", "acid green": "b0bf1a", "aliceblue": "#f0f8ff",
-                 "alizarin crimson": "#e32636", "amaranth": "#e52b50", "amber": "	#ffbf00", "aqua": "#00ffff",
+COLOUR_TO_HEX = {"absolute zero": "#0048ba", "acid green": "#b0bf1a", "aliceblue": "#f0f8ff",
+                 "alizarin crimson": "#e32636", "amaranth": "#e52b50", "amber": "#ffbf00", "aqua": "#00ffff",
                  "apricot": "#fbceb1", "beaver": "#9f8170", "black": "#000000"}
 
 colour = input("Colour name: ").lower()
