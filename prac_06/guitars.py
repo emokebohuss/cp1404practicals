5d999f0211ccd8652e0eac28d9e12800a2d419fe
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-10-29 21:40:51 +1000

    Complete program with getting and storing guitar details

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -9,14 +9,16 @@ CURRENT_YEAR = 2022
 
 print("My guitars!")
 guitars = []
-# name = input("Name: ")
-# while name != "":
-guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    new_guitar = Guitar(name, year, cost)
+    guitars.append(new_guitar)
+    print(f"{new_guitar} added.\n")
+    name = input("Name: ")
 
 print(f"These are my guitars:")
 for i, guitar in enumerate(guitars, 1):
     vintage_string = " (vintage)" if guitar.is_vintage(CURRENT_YEAR) else ""
     print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
-
-
