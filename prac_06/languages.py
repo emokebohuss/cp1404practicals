c0cc5189f44dc3cc9482969b6c01f7935258b201
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-10-29 19:13:24 +1000

    Add languages.py and import ProgrammingLanguage class

---
+++ prac_06/languages.py
@@ -0,0 +1,12 @@
+"""
+CP1404 Practical 6 - Client code to use the ProgrammingLanguage class.
+Estimated time: 20 mins
+Actual time: 14 mins
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
