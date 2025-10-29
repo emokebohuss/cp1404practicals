a41949717defb6fb2fc47c79fe2fa2593b54645f
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-10-29 19:16:31 +1000

    Add str method to return string form of ProgrammingLangugae data

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -14,3 +14,7 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
+
+    def __str__(self):
+        """Return string representation of data in ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
