c54e582b8563f61f9310b78786267023a49a8703
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-11-09 23:39:42 +1000

    Complete load_projects function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -8,26 +8,26 @@ from project import Project
 
 FILENAME = "projects.txt"
 
-# def main():
-#     projects = load_projects(FILENAME)
-#     print(projects[1])
-#
-#
-# def load_projects(filename):
-#     """Load guitars from CSV file into a list of Guitar objects."""
-#     projects = []
-#     with open(filename, 'r') as in_file:
-#         for line in in_file:
-#             parts = line.strip().split("    ")
-#             print(parts)
-#             project = Project(parts[0], int(parts[1]), float(parts[2]))
-#             projects.append(project)
-#     return projects
 
-with open(FILENAME, 'r') as in_file:
-    in_file.readline()
-    for line in in_file:
-        parts = line.strip().split("\t")
-        parts[1] = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()
-        project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
-        print(project)
+def main():
+    projects = load_projects(FILENAME)
+    print(projects[1])
+
+
+def load_projects(filename):
+    """Load projects from file into a list of Project objects."""
+    projects = []
+    with open(filename, 'r') as in_file:
+        in_file.readline()  # remove header
+        for line in in_file:
+            parts = line.strip().split("\t")  # handle tab delimited txt file data
+            # Convert date to datetime.date
+            parts[1] = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()
+            # Construct a Project object using the elements
+            project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
+            projects.append(project)
+    return projects
+
+
+if __name__ == '__main__':
+    main()
