3d81220a8350bd90f9c177a72b0f7956e333826e
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-11-10 00:23:10 +1000

    Add __lt__ method to Project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,6 +1,6 @@
 """project.py for Project class
 Estimate: 12 mins
-Actual: 8 +
+Actual: 8 + 4.21
 """
 
 
@@ -8,7 +8,7 @@ class Project:
     """Represent a Project object."""
 
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
-        """Initialise a Guitar instance."""
+        """Initialise a Project instance."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -17,5 +17,13 @@ class Project:
 
     def __str__(self):
         """Represent Project data as a string."""
-        return (f"{self.name} started on {self.start_date}, priority {self.priority}, estimated: "
-                f"${self.cost_estimate:,.2f}, completed % {self.completion_percentage}")
+        return (f"{self.name} start: {self.start_date}, priority {self.priority}, estimate: "
+                f"${self.cost_estimate:,.2f}, completion: {self.completion_percentage}%")
+
+    def __lt__(self, other):
+        """Determine whether priority of Project instance is lower than other instance's."""
+        return self.priority < other.priority
+
+    def is_completed(self):
+        """Determine if project is 100% complete."""
+        return self.completion_percentage == 100
