0e62425632510b5ed4e6a86b2d738c92704ff4c2
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-11-16 23:29:46 +1000

    Add init and build methods

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -3,3 +3,23 @@ CP1404 Prac 8
 By Emoke Bohuss
 Dynamic Labels
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+
+class DynamicWidgetsApp(App):
+    """Main program - Kivy app for dynamic label creation."""
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        self.names = ["Bob Brown", "Cat Cyan", "Oren Ochre"]
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        return self.root
