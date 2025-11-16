286f92f245cf3939c9b933e0ae1f586d00dd957e
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-11-16 23:50:07 +1000

    Add create label method

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -3,10 +3,10 @@ CP1404 Prac 8
 By Emoke Bohuss
 Dynamic Labels
 """
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
+from kivy.uix.label import Label
 
 
 class DynamicWidgetsApp(App):
@@ -22,4 +22,17 @@ class DynamicWidgetsApp(App):
         """Build the Kivy GUI."""
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
         return self.root
+
+    def create_labels(self):
+        """Create labels from data and add them to the GUI."""
+        for name in self.names:
+            # create a label for each data entry, specifying the text
+            temp_label = Label(text=name)
+            # add the button to the "id" layout widget
+            temp_label.color = (1, 0, 1, 1)
+            self.root.ids.main.add_widget(temp_label)
+
+
+DynamicWidgetsApp().run()
