07c3f276662254bdaf4a8fe5099a0c1694d46c00
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-11-20 00:00:57 +1000

    Write car instances and run tests on each

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,9 +1,31 @@
 """
 CP1404 Prac 9
 Emoke Bohuss
-Client code for unreliable car
+Client (test) code for unreliable car
 """
 from prac_09.unreliable_car import UnreliableCar
 
-my_car = UnreliableCar(name="Mustang", fuel=100, reliability=50)
-print(my_car)
+
+def run_tests():
+    """Run tests for UnreliableCar instances with various reliability"""
+    mid_car = UnreliableCar("Ranger", 100, 50)
+    good_car = UnreliableCar("Mazda", 100, 90)
+    bad_car = UnreliableCar("BMW", 100, 10)
+    print_successful_drive_percentage(mid_car)
+    print_successful_drive_percentage(good_car)
+    print_successful_drive_percentage(bad_car)
+
+
+def print_successful_drive_percentage(car):
+    """Attempt to drive 1km 100 times and display % of successful attempts."""
+    no_of_successful_drives = 0
+    for i in range(100):
+        fuel = car.fuel
+        car.drive(1)
+        if car.fuel < fuel:
+            no_of_successful_drives += 1
+    chance = no_of_successful_drives / 100
+    print(f"{car.name} with {car.reliability}% reliability successfully drove {chance * 100:.2f}% of attempts.")
+
+
+run_tests()
