fbf7cae60f43e7ea2870494f31cd184eb3793cf2
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-11-19 23:59:38 +1000

    Complete specialised drive method

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -3,6 +3,7 @@ CP1404 Prac 9
 Emoke Bohuss
 UnreliableCar class
 """
+import random
 
 from prac_09.car import Car
 
@@ -10,10 +11,9 @@ from prac_09.car import Car
 class UnreliableCar(Car):
     """Specialised version of a Car that includes reliability."""
 
-    def __init__(self, reliability, **kwargs):
+    def __init__(self, name, fuel, reliability):
         """Initialise an UnreliableCar instance, based on parent class Car."""
-        super().__init__(**kwargs)
-        self.reliability = 0.0
+        super().__init__(name, fuel)
         self.reliability = reliability
 
     def __str__(self):
@@ -24,9 +24,13 @@ class UnreliableCar(Car):
         """Return string representation of an UnreliableCar instance."""
         return str(vars(self))
 
+    def drive(self, distance):
+        """Drive the car a given distance only if a randomly generated number is less than the car's reliability.
 
-
-
-
-
-
+        Drive given distance if car has enough fuel
+        or drive until fuel runs out, return the distance actually driven.
+        """
+        if random.randint(0, 100) < self.reliability:
+            return super().drive(distance)
+        else:
+            return 0
