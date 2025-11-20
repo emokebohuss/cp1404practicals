7ec18f184a8fe31e2c45d04d41ec9737fe836134
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-11-20 12:05:40 +1000

    Fix typo in string method to match example output

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -1,6 +1,6 @@
 """
 CP1404 Practical 9
-Taxi class
+SilverServiceTaxi class
 """
 from taxi import Taxi
 
@@ -9,7 +9,7 @@ class SilverServiceTaxi(Taxi):
     """Specialised version of a Taxi that includes fanciness and flagfall."""
     flagfall = 4.50
 
-    def __init__(self, name, fuel, fanciness=0.0):
+    def __init__(self, name, fuel, fanciness):
         """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
@@ -17,7 +17,7 @@ class SilverServiceTaxi(Taxi):
 
     def __str__(self):
         """Return a string like a Taxi but with flagfall."""
-        return f"{super().__str__()} plus flagfall of ${self.flagfall}."
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}."
 
     def __repr__(self):
         """Return string representation of data in a SilverServiceTaxi."""
