9456e5fc3ea2fbe2dbe39c3b7d205bb6448b35d3
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-11-21 03:31:28 +1000

    Add choose taxi menu option

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -12,17 +12,19 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
+    total_bill = 0.0
     print("Let's drive!")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            print("choice")
+            current_taxi = choose_taxi(taxis)
         elif choice == "D":
-            print("drive")
+            total_bill = drive_taxi(current_taxi, total_bill)
         else:
             print("Invalid option")
-        print(f"Bill to date: ")
+        print(f"Bill to date: ${total_bill:.2f}")
+        print()
         print(MENU)
         choice = input(">>> ").upper()
     print(f"Total trip cost: ")
@@ -31,6 +33,15 @@ def main():
 def display_taxis(taxis):
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
+
+
+def choose_taxi(taxis):
+    print("Taxis available:")
+    display_taxis(taxis)
+    chosen_taxi = int(input("Choose taxi: "))
+    current_taxi = taxis[chosen_taxi]
+    print(current_taxi)
+    return current_taxi
 
 
 main()
