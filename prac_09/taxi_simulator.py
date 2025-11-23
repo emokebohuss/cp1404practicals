f186171bddec1f819409cd3a6ff2bbc78b583e16
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-11-23 21:27:42 +1000

    Merge get_valid_taxi function into choose taxi function and add docstrings to functions

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -10,6 +10,8 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
+    """Main for taxi_simulator program. Allow user to choose taxis and drive them, calculating the fare for each trip
+    and the total bill."""
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
     total_bill = 0.0
@@ -18,13 +20,14 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            current_taxi = choose_taxi(taxis)
+            chosen_taxi = choose_taxi(taxis)
+            if chosen_taxi is not None:
+                current_taxi = chosen_taxi
         elif choice == "D":
             total_bill = drive_taxi(current_taxi, total_bill)
         else:
             print("Invalid option")
         print(f"Bill to date: ${total_bill:.2f}")
-        print()
         print(MENU)
         choice = input(">>> ").upper()
     print(f"Total trip cost: ${total_bill:.2f}")
@@ -33,18 +36,29 @@ def main():
 
 
 def display_taxis(taxis):
+    """Display taxis and their details."""
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
 
 
 def choose_taxi(taxis):
+    """Get valid taxi index from user and return chosen taxi."""
     print("Taxis available:")
     display_taxis(taxis)
-    current_taxi = get_valid_taxi(taxis)
-    return current_taxi
+    try:
+        taxi_index = int(input("Choose taxi: "))
+        chosen_taxi = taxis[taxi_index]
+        return chosen_taxi
+    except ValueError:
+        print("Invalid taxi choice")
+    except IndexError:
+        print("Invalid taxi choice")
 
 
 def drive_taxi(current_taxi, total_bill):
+    """Complete a drive taxi interaction.
+    If no taxi is selected, display error message. If taxi is selected, drive given distance, display taxi and trip
+     details, then calculate and return the total bill including this trip's fare."""
     if current_taxi is None:
         print("You need to choose a taxi before you can drive")
     else:
@@ -56,19 +70,8 @@ def drive_taxi(current_taxi, total_bill):
     return total_bill
 
 
-def get_valid_taxi(taxis):
-    """Get valid taxi index from user and return chosen taxi."""
-    try:
-        taxi_index = int(input("Choose taxi: "))
-        current_taxi = taxis[taxi_index]
-        return current_taxi
-    except ValueError:
-        print("Invalid taxi choice")
-    except IndexError:
-        print("Invalid taxi choice")
-
-
 def get_valid_distance():
+    """Get positive integer and return as distance."""
     is_valid_input = False
     while not is_valid_input:
         try:
