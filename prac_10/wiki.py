6df81c939415221b99c8e9894a4eaf04f300bf8a
emokebohuss <emokebianka.bohuss@my.jcu.edu.au>
2025-12-03 02:14:26 +1000

    Add exception handling

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,16 +4,19 @@ Wikipedia
 """
 
 import wikipedia
-from wikipedia import PageError
+from wikipedia import PageError, DisambiguationError
 
-search_string = input("Page title: ")
+search_string = input("Enter page title: ")
 while search_string != "":
     try:
-        page_title = wikipedia.page(search_string)
+        page_title = wikipedia.page(search_string, auto_suggest=False)
         print(page_title.title)
+        print(wikipedia.summary(search_string))
         print(page_title.url)
-        print(page_title.images[0])
-        print(search_string)
     except PageError:
-        print(f"No results for {search_string}")
-    search_string = input("Page title: ")
+        print(f'Page id "{search_string}" does not match any pages. Try another id!')
+    except DisambiguationError as page_title_options:
+        print("We need a more specific title. Try one of the following, or a new search:")
+        print(page_title_options.options)
+    search_string = input("\nEnter page title: ")
+print("Thank you.")
